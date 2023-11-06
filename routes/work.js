071bac8a3cccf40b870466cafdc4f50b1ad13cd0
HappyMan17 <sebasgrisales16@gmail.com>
2023-11-06 18:53:31 -0500

    feat: Endpoint to get work by id created.

--- routes/work.js
+++ routes/work.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllWorks, addWork } from '../service/workService.js';
+import { getAllWorks, addWork, getWorkById } from '../service/workService.js';
 // import { getAllWorks, addWork, updateWork, deleteWork, getWorkNameById } from '../service/workService.js';
 
 const router = express.Router();
@@ -16,6 +16,15 @@ router.get('/all', (req, res) => {
  */
 router.post('/add', (req, res) => {
   addWork(req, res);
+})
+
+/**
+ * Must always be at the end of the file
+ * Get work by id
+ * 
+ */
+router.get('/:id', (req, res) => {
+  getWorkById(req, res);
 })
 
 export default router;
