850a79004fcb20e362483b46228d3326c3ac40c1
HappyMan17 <sebasgrisales16@gmail.com>
2023-11-06 20:27:56 -0500

    feat: host variable added to the pool config.

--- routes/config.js
+++ routes/config.js
@@ -1,10 +1,11 @@
 import { envs } from "../config/envs.js";
 
 const config = {
-    "user": envs.POSTGRES_USER, //env var: PGUSER
-    "database": envs.POSTGRES_DB, //env var: PGDATABASE
-    "password": envs.POSTGRES_PASSWORD, //env var: PGPASSWORD
-    "port": envs.DB_PORT //env var: PGPORT
+  "host": 'localhost', //env var: PGHOST
+  "user": envs.POSTGRES_USER, //env var: PGUSER
+  "database": envs.POSTGRES_DB, //env var: PGDATABASE
+  "password": envs.POSTGRES_PASSWORD, //env var: PGPASSWORD
+  "port": envs.DB_PORT //env var: PGPORT
 };
   
 export default config;
